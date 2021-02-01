953a440ef55e7e18bef088f62f1063df3c9be011
abdellik <AbdelliK@bender.muc.lv1871.de>
2021-02-01 21:30:15 +0100

    add_@lazy_annotation

--- challenge-java-broken/src/main/java/com/celonis/challenge/services/TaskService.java
+++ challenge-java-broken/src/main/java/com/celonis/challenge/services/TaskService.java
@@ -1,72 +1,72 @@
 package com.celonis.challenge.services;
 
-import com.celonis.challenge.exceptions.InternalException;
-import com.celonis.challenge.exceptions.NotFoundException;
-import com.celonis.challenge.model.ProjectGenerationTask;
-import com.celonis.challenge.model.ProjectGenerationTaskRepository;
-import org.springframework.stereotype.Service;
-
 import java.net.URL;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
+import com.celonis.challenge.exceptions.InternalException;
+import com.celonis.challenge.exceptions.NotFoundException;
+import com.celonis.challenge.model.ProjectGenerationTask;
+import com.celonis.challenge.model.ProjectGenerationTaskRepository;
+
 @Service
 public class TaskService {
 
-    private final ProjectGenerationTaskRepository projectGenerationTaskRepository;
+	private final ProjectGenerationTaskRepository projectGenerationTaskRepository;
 
-    private final FileService fileService;
+	private final FileService fileService;
 
+	// add @Lazy to fix Circular dependencies Problem
+	public TaskService(ProjectGenerationTaskRepository projectGenerationTaskRepository, @Lazy FileService fileService) {
+		this.projectGenerationTaskRepository = projectGenerationTaskRepository;
+		this.fileService = fileService;
+	}
 
-    public TaskService(ProjectGenerationTaskRepository projectGenerationTaskRepository,
-                       FileService fileService) {
-        this.projectGenerationTaskRepository = projectGenerationTaskRepository;
-        this.fileService = fileService;
-    }
+	public List<ProjectGenerationTask> listTasks() {
+		return projectGenerationTaskRepository.findAll();
+	}
 
+	public ProjectGenerationTask createTask(ProjectGenerationTask projectGenerationTask) {
+		projectGenerationTask.setId(null);
+		projectGenerationTask.setCreationDate(new Date());
+		return projectGenerationTaskRepository.save(projectGenerationTask);
+	}
 
-    public List<ProjectGenerationTask> listTasks() {
-        return projectGenerationTaskRepository.findAll();
-    }
+	public ProjectGenerationTask getTask(String taskId) {
+		return get(taskId);
+	}
 
-    public ProjectGenerationTask createTask(ProjectGenerationTask projectGenerationTask) {
-        projectGenerationTask.setId(null);
-        projectGenerationTask.setCreationDate(new Date());
-        return projectGenerationTaskRepository.save(projectGenerationTask);
-    }
+	public ProjectGenerationTask update(String taskId, ProjectGenerationTask projectGenerationTask) {
+		ProjectGenerationTask existing = get(taskId);
+		existing.setCreationDate(projectGenerationTask.getCreationDate());
+		existing.setName(projectGenerationTask.getName());
+		return projectGenerationTaskRepository.save(existing);
+	}
 
-    public ProjectGenerationTask getTask(String taskId) {
-        return get(taskId);
-    }
+	public void delete(String taskId) {
+		projectGenerationTaskRepository.delete(taskId);
+	}
 
-    public ProjectGenerationTask update(String taskId, ProjectGenerationTask projectGenerationTask) {
-        ProjectGenerationTask existing = get(taskId);
-        existing.setCreationDate(projectGenerationTask.getCreationDate());
-        existing.setName(projectGenerationTask.getName());
-        return projectGenerationTaskRepository.save(existing);
-    }
+	public void executeTask(String taskId) {
+		URL url = Thread.currentThread().getContextClassLoader().getResource("challenge.zip");
+		if (url == null) {
+			throw new InternalException("Zip file not found");
+		}
+		try {
+			fileService.storeResult(taskId, url);
+		} catch (Exception e) {
+			throw new InternalException(e);
+		}
+	}
 
-    public void delete(String taskId) {
-        projectGenerationTaskRepository.delete(taskId);
-    }
-
-    public void executeTask(String taskId) {
-        URL url = Thread.currentThread().getContextClassLoader().getResource("challenge.zip");
-        if (url == null) {
-            throw new InternalException("Zip file not found");
-        }
-        try {
-            fileService.storeResult(taskId, url);
-        } catch (Exception e) {
-            throw new InternalException(e);
-        }
-    }
-
-    private ProjectGenerationTask get(String taskId) {
-        ProjectGenerationTask projectGenerationTask = projectGenerationTaskRepository.findOne(taskId);
-        if (projectGenerationTask == null) {
-            throw new NotFoundException();
-        }
-        return projectGenerationTask;
-    }
+	private ProjectGenerationTask get(String taskId) {
+		ProjectGenerationTask projectGenerationTask = projectGenerationTaskRepository.findOne(taskId);
+		if (projectGenerationTask == null) {
+			throw new NotFoundException();
+		}
+		return projectGenerationTask;
+	}
 }
